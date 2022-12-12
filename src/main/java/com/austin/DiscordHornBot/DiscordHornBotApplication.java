0c35fde8505578bbe0c6502ce0de2b41b95e5ba4
TheAustinBae <austin.bae@rutgers.edu>
2022-12-12 18:57:33 -0500

    first commit

---
+++ src/main/java/com/austin/DiscordHornBot/DiscordHornBotApplication.java
@@ -0,0 +1,13 @@
+package com.austin.DiscordHornBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordHornBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordHornBotApplication.class, args);
+	}
+
+}
